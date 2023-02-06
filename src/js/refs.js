f968d7f575d1cd9195aa45ce2da165693b403c9d
Zhenya240315 <105649502+Zhenya240315@users.noreply.github.com>
2023-02-06 22:31:40 +0200

    fin-hw-11

---
+++ src/js/refs.js
@@ -0,0 +1,8 @@
+export const refs = {
+  form: document.querySelector('.search-form'),
+  input: document.querySelector('.search-term'),
+  searchBnt: document.querySelector('.search-button'),
+  loadBnt: document.querySelector('[data-load]'),
+  list: document.querySelector('.gallery'),
+  select: document.querySelector('#perPage'),
+};
---
+++ src/js/service.js
@@ -0,0 +1,62 @@
+import { refs } from './refs';
+import axios from 'axios';
+
+// const axios = require('axios').default;
+const API_KEY = '31865177-941795f6a9b4b09e4f4c303b4';
+const BASE_URL = 'https://pixabay.com/api/';
+const parameters = 'image_type=photo&orientation=horizontal&safesearch=true';
+
+export default class ApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.totalPages = 0;
+    this.perPage = 40;
+  }
+
+  async fetchArticles() {
+    const query = await axios.get(
+      `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&${parameters}&per_page=${this.perPage}&page=${this.page}`
+    );
+    console.log(query.data.hits.length);
+    return await query.data;
+  }
+
+  incrementPage() {
+    this.page += 1;
+    console.log(this.page);
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+
+  get isShownLoadMoreBtn() {
+    return this.page < this.totalPages;
+  }
+
+  calckTotalPages(total) {
+    return (this.totalPages = Math.ceil(total / this.perPage));
+  }
+
+  // // Notify.info(`показано: ${apiService.incrementImg(data.totalHits)}`);
+  incrementImg(total) {
+    let result = 0;
+    if (this.page < this.totalPages) {
+      result = this.page * this.perPage;
+    }
+    if (this.page === this.totalPages) {
+      result = (this.page - 1) * this.perPage + (total % this.perPage);
+    }
+    console.log(result);
+    return result;
+  }
+}
